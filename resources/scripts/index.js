340fc2c53cd0c27ca6fb4fdf7d246dba39cb3d44
pratxks <101081557+pratxks@users.noreply.github.com>
2022-11-29 00:41:40 +0530

    finished transpilling

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,3 +1,4 @@
+import { RockPaperScissors } from './rock_paper_scissors.js';
 // Elements
 const welcomeScreen = document.getElementById(`welcome-screen`);
 const gameScreen = document.getElementById(`game-screen`);
@@ -10,56 +11,56 @@ const gameHistoryParagraph = document.getElementById(`game-history`);
 const resetGameButton = document.getElementById(`reset-game-button`);
 
 // instantiate the game object from the `RockPaperScissors` class.
-var game;
-var username;
+let game;
+let username;
 
 // hide gamescreen
 gameScreen.classList.add(`d-none`);
 resetGameButton.classList.add(`d-none`);
 
 // updateScoreTallyUI
-function updateScoreTallyUI(){
-  scoreParagraph.innerHTML = `${game.username}: ${game.score.user} v CPU: ${game.score.cpu}`;
+function updateScoreTallyUI() {
+   scoreParagraph.innerHTML = `${game.username}: ${game.score.user} v CPU: ${game.score.cpu}`;
 }
 
 // updateGameHistoryUI
-function updateGameHistoryUI(){
-      gameHistoryParagraph.innerHTML = ''; 
-    gameHistoryParagraph.innerHTML = game.gameHistoryLog; 
+function updateGameHistoryUI() {
+   gameHistoryParagraph.innerHTML = ``;
+   gameHistoryParagraph.innerHTML = game.gameHistoryLog;
 }
 
 // start-game-button EventListener
 startGameButton.addEventListener(`click`, function (e) {
-  e.preventDefault(); 
-     username = userName.value;
-  game = new RockPaperScissors(username);
+   e.preventDefault();
+   username = userName.value;
+   game = new RockPaperScissors(username);
 
-  welcomeScreen.classList.add(`d-none`);
-  gameScreen.classList.remove(`d-none`);
-     resetGameButton.classList.remove(`d-none`);
-  updateScoreTallyUI();
-     updateGameHistoryUI();
-  // Complete
+   welcomeScreen.classList.add(`d-none`);
+   gameScreen.classList.remove(`d-none`);
+   resetGameButton.classList.remove(`d-none`);
+   updateScoreTallyUI();
+   updateGameHistoryUI();
+   // Complete
 });
 
 // go-button EventListener
 goButton.addEventListener(`click`, function (e) {
-  e.preventDefault(); 
-  const selectedOption = userSelection.options[userSelection.selectedIndex].value;
-  game.play(userSelection.value); 
-  updateScoreTallyUI(); 
-  updateGameHistoryUI();
-  
+   e.preventDefault();
+   const selectedOption = userSelection.options[userSelection.selectedIndex].value;
+   game.play(userSelection.value);
+   updateScoreTallyUI();
+   updateGameHistoryUI();
+
 });
 
 //reset-game-button
-resetGameButton.addEventListener('click', function(e) { 
-     e.preventDefault();
-  userName.value = '';
-     username = '';
-  game = new RockPaperScissors(username);
+resetGameButton.addEventListener(`click`, function (e) {
+   e.preventDefault();
+   userName.value = ``;
+   username = ``;
+   game = new RockPaperScissors(username);
 
-  welcomeScreen.classList.remove(`d-none`);
-     gameScreen.classList.add(`d-none`);
-  resetGameButton.classList.add(`d-none`);
+   welcomeScreen.classList.remove(`d-none`);
+   gameScreen.classList.add(`d-none`);
+   resetGameButton.classList.add(`d-none`);
 })
