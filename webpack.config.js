8bd127ff7fb98dc2d7ef06649c2840f492f3be43
pratxks <101081557+pratxks@users.noreply.github.com>
2022-11-29 00:47:20 +0530

    configured jest

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require(`path`);
 
 module.exports = {
-  entry: `./src/resources/scripts/index.js`,
+  entry: `./resources/scripts/index.js`,
   output: {
     filename: `main.js`,
     path: path.resolve(__dirname, `dist`),
